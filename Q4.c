7a615d8aad83a1be7a25ad5892662bad8840e181
snehsriv <snehsri99@gmail.com>
2020-04-13 23:13:48 +0530

    Updated Code

--- Q4.c
+++ Q4.c
@@ -18,12 +18,14 @@ int main()
 	pid = fork();
 	if (pid==0)
 		{
+                        printf("Child Process\n");
 			printf("Message sent in Child process (pid = %d and ppid = %d): %s\n", getpid(), getppid(), message1);
-			write(pipe1[1], message1, sizeof(message1));	
+			write(pipe1[1], message1, sizeof(message1));
 		}
 	else
 		{
 			read( pipe1[0], read_message, sizeof(read_message));
+			printf("Parent Process\n");
 			printf("%s (pid = %d and ppid = %d) and message received: %s\n", message2, getpid(), getppid(), read_message);
 		}
 	return 0;
